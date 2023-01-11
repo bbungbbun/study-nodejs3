e40ccf3d718dbed2137ffde7e667b1837c7ad59d
ynnotun <flexible.yuni@gmail.com>
2023-01-11 14:45:41 +0900

    express 시작

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("여기는 루트")
+});
+app.get("/login", (req, res)=>{
+    res.send("여기는 로그인 화면")
+});
 app.listen(3000, function(){
     console.log('hi');
-})
+});
