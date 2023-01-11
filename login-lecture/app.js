fb57985c2ab705ef2b72b895c92c11d4dad73be0
ynnotun <flexible.yuni@gmail.com>
2023-01-11 15:46:59 +0900

    app.listen() 모듈화

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -4,8 +4,6 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
-
 // 라우팅
 const home = require("./routes/home");
 
@@ -14,6 +12,4 @@ app.set("view engine", "ejs");
 
 app.use("/", home); // use -> 미들웨어를 등록해주는 메소드
 
-app.listen(PORT, function(){
-    console.log('서버가동');
-});
+module.exports = app;
